ea937cf50ca99c44681c6f2deaa60584d78483cf
Peter Ma <peterma@Peters-MacBook-Pro.local>
2018-07-16 18:09:16 -0700

    wifi now works

--- IoTHubAmqp/IoTHubAmqp/Program.cs
+++ IoTHubAmqp/IoTHubAmqp/Program.cs
@@ -4,18 +4,21 @@ using System;
 using System.Diagnostics;
 using System.Text;
 using System.Threading;
-#if NETMF
 using Microsoft.SPOT;
-#endif
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Net.NetworkInformation;
+using System.Net;
+using SecretLabs.NETMF.Hardware.Netduino;
+
 
 namespace IoTHubAmqp
 {
     class Program
     {
-        private const string HOST = "[IOT_HUB_NAME].azure-devices.net";
+        private const string HOST = "IoTApps.azure-devices.net";
         private const int PORT = 5671;
-        private const string DEVICE_ID = "[DEVICE_ID]";
-        private const string DEVICE_KEY = "[DEVICE_KEY]";
+        private const string DEVICE_ID = "smartplant1";
+        private const string DEVICE_KEY = "+KPMW6B/55LF/G7ti7ffr5I5IysYueyRVigkcWzA3dw=";
 
         private static Address address;
         private static Connection connection;
@@ -23,22 +26,37 @@ namespace IoTHubAmqp
 
         private static Thread receiverThread;
 
+        private static NetworkInterface[] _interfaces;
+
         static void Main(string[] args)
         {
-            /*
-            Amqp.Trace.TraceLevel = Amqp.TraceLevel.Frame | Amqp.TraceLevel.Verbose;
-#if NETMF
-            Amqp.Trace.TraceListener = (f, a) => Debug.Print(DateTime.Now.ToString("[hh:ss.fff]") + " " + Fx.Format(f, a));
-#else
-            Amqp.Trace.TraceListener = (f, a) => Debug.Print(DateTime.Now.ToString("[hh:ss.fff]") + " " + Fx.Format(f, a));
-#endif
-*/
+            Debug.Print("Start");
+            _interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            if(!InitializeNetwork())
+            {
+                return;
+            }
+
+//          Debug.Print("IP Address: " + IPAddress.GetDefaultLocalAddress().ToString();
+            Debug.Print("Ready");
+
             address = new Address(HOST, PORT, null, null);
-            connection = new Connection(address);
+
+            try{
+                connection = new Connection(address);                
+            }
+            catch(Exception e)
+            {
+                Debug.Print(e.Message);    
+            }
+
+
 
             string audience = Fx.Format("{0}/devices/{1}", HOST, DEVICE_ID);
             string resourceUri = Fx.Format("{0}/devices/{1}", HOST, DEVICE_ID);
-            
+            Debug.Print("second");
+
             string sasToken = GetSharedAccessSignature(null, DEVICE_KEY, resourceUri, new TimeSpan(1, 0, 0));
             bool cbs = PutCbsToken(connection, HOST, sasToken, audience);
 
@@ -50,6 +68,7 @@ namespace IoTHubAmqp
                 receiverThread = new Thread(ReceiveCommands);
                 receiverThread.Start();
             }
+            Debug.Print("Thread");
 
             // just as example ...
             // the application ends only after received a command or timeout on receiving
@@ -58,6 +77,171 @@ namespace IoTHubAmqp
             session.Close();
             connection.Close();
         }
+
+        private static bool InitializeNetwork()
+        {
+            if (Microsoft.SPOT.Hardware.SystemInfo.SystemID.SKU == 3)
+            {
+                Debug.Print("Wireless tests run only on Device");
+                return false;
+            }
+
+            Debug.Print("Getting all the network interfaces.");
+            _interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            // debug output
+            ListNetworkInterfaces();
+
+            // loop through each network interface
+            foreach (var net in _interfaces)
+            {
+
+                // debug out
+                //ListNetworkInfo(net);
+
+                switch (net.NetworkInterfaceType)
+                {
+                    case (NetworkInterfaceType.Ethernet):
+                        Debug.Print("Found Ethernet Interface");
+                        break;
+                    case (NetworkInterfaceType.Wireless80211):
+                        Debug.Print("Found 802.11 WiFi Interface");
+                        break;
+                    case (NetworkInterfaceType.Unknown):
+                        Debug.Print("Found Unknown Interface");
+                        break;
+                }
+
+                // check for an IP address, try to get one if it's empty
+                return CheckIPAddress(net);
+            }
+
+            // if we got here, should be false.
+            return false;
+        }
+
+        private static void ListNetworkInterfaces()
+        {
+            foreach (var net in _interfaces)
+            {
+                switch (net.NetworkInterfaceType)
+                {
+                    case (NetworkInterfaceType.Ethernet):
+                        Debug.Print("Found Ethernet Interface");
+                        break;
+                    case (NetworkInterfaceType.Wireless80211):
+                        Debug.Print("Found 802.11 WiFi Interface");
+                        break;
+                    case (NetworkInterfaceType.Unknown):
+                        Debug.Print("Found Unknown Interface");
+                        break;
+
+                }
+            }
+        }
+
+
+        private static bool CheckIPAddress(NetworkInterface net)
+        {
+            int timeout = 10000; // timeout, in milliseconds to wait for an IP. 10,000 = 10 seconds
+            //Debug.Print(net.IPAddress);
+            Debug.Print("getting ip");
+            // check to see if the IP address is empty (0.0.0.0). IPAddress.Any is 0.0.0.0.
+            Thread.Sleep(5000);
+
+            if (net.IPAddress == IPAddress.Any.ToString())
+            {
+                Debug.Print("No IP Address");
+
+                if (net.IsDhcpEnabled)
+                {
+                    Debug.Print("DHCP is enabled, attempting to get an IP Address");
+
+                    // ask for an IP address from DHCP [note this is a static, not sure which network interface it would act on]
+                    int sleepInterval = 10;
+                    int maxIntervalCount = timeout / sleepInterval;
+                    int count = 0;
+                    while (IPAddress.GetDefaultLocalAddress() == IPAddress.Any && count < maxIntervalCount)
+                    {
+                        Debug.Print("Sleep while obtaining an IP");
+                        Thread.Sleep(10);
+                        count++;
+                    };
+
+                    // if we got here, we either timed out or got an address, so let's find out.
+                    if (net.IPAddress == IPAddress.Any.ToString())
+                    {
+                        Debug.Print("Failed to get an IP Address in the alotted time.");
+                        return false;
+                    }
+
+                    Debug.Print("Got IP Address: " + net.IPAddress.ToString());
+                    ListNetworkInfo(net);
+
+                    return true;
+
+                    //NOTE: this does not work, even though it's on the actual network device. [shrug]
+                    // try to renew the DHCP lease and get a new IP Address
+                    //net.RenewDhcpLease ();
+                    //while (net.IPAddress == "0.0.0.0") {
+                    //  Thread.Sleep (10);
+                    //}
+
+                }
+                else
+                {
+                    Debug.Print("DHCP is not enabled, and no IP address is configured, bailing out.");
+                    return false;
+                }
+            }
+            else
+            {
+                Debug.Print("Already had IP Address: " + net.IPAddress.ToString());
+                return true;
+            }
+
+        }
+
+        static protected void ListNetworkInfo(NetworkInterface net)
+        {
+            Debug.Print("MAC Address: " + BytesToHexString(net.PhysicalAddress));
+            Debug.Print("DHCP enabled: " + net.IsDhcpEnabled.ToString());
+            Debug.Print("Dynamic DNS enabled: " + net.IsDynamicDnsEnabled.ToString());
+
+
+            if (net is Wireless80211)
+            {
+                var wifi = net as Wireless80211;
+                Debug.Print("SSID:" + wifi.Ssid.ToString());
+            }
+            Debug.Print("IP Address: " + net.IPAddress.ToString());
+            Debug.Print("Subnet Mask: " + net.SubnetMask.ToString());
+            Debug.Print("Gateway: " + net.GatewayAddress.ToString());
+        }
+
+        private static string BytesToHexString(byte[] bytes)
+        {
+            string hexString = string.Empty;
+
+            // Create a character array for hexidecimal conversion.
+            const string hexChars = "0123456789ABCDEF";
+
+            // Loop through the bytes.
+            for (byte b = 0; b < bytes.Length; b++)
+            {
+                if (b > 0)
+                    hexString += "-";
+
+                // Grab the top 4 bits and append the hex equivalent to the return string.        
+                hexString += hexChars[bytes[b] >> 4];
+
+                // Mask off the upper 4 bits to get the rest of it.
+                hexString += hexChars[bytes[b] & 0x0F];
+            }
+
+            return hexString;
+        }
+
 
         static private void SendEvent()
         {
@@ -84,7 +268,7 @@ namespace IoTHubAmqp
             Message received = receiveLink.Receive();
             if (received != null)
                 receiveLink.Accept(received);
-                
+
             receiveLink.Close();
         }
 
@@ -168,6 +352,6 @@ namespace IoTHubAmqp
                     HttpUtility.UrlEncode(expiry));
             }
         }
-        
+
     }
 }
